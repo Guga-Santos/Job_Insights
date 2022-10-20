c2fcb9e20102766c53529960393353e9f9de3268
GugaSantos <guga.santos.guga@gmail.com>
2022-10-20 15:47:27 -0300

    feat: ensure its possible to get unique industries

--- src/insights.py
+++ src/insights.py
@@ -44,6 +44,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
+    data = read(path)
+    unique_industries = set()
+    for object in data:
+        if object['industry'] != "":
+            unique_industries.add(object['industry'])
+    return unique_industries
     """Checks all different industries and returns a list of them
 
     Must call `read`
