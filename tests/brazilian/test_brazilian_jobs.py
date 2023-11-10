d5301e07ddf03653d495504219285ae2648c6259
GugaSantos <guga.santos.guga@gmail.com>
2023-11-10 14:43:55 -0300

    feat: delete trybe files

--- tests/brazilian/test_brazilian_jobs.py
+++
@@ -1,8 +0,0 @@
-from src.brazilian_jobs import read_brazilian_file
-
-
-def test_brazilian_jobs():
-    test = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
-    for object in test:
-        assert 'title' and 'salary' and 'type' in object
-        assert 'titulo' and 'salario' and 'tipo' not in object
--- tests/counter/test_counter.py
+++
@@ -1,6 +0,0 @@
-from src.counter import count_ocurrences
-
-
-def test_counter():
-    test = count_ocurrences('src/jobs.csv', 'jobs')
-    assert test == 306
